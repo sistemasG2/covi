278d6bca47237747c7860ba0df3b09ef4ec875e7
Eduardo Alfaro G2 <soporte@sistemasg2.com>
2017-12-14 11:19:07 -0600

    Create Accounts Table
    Create migration for table accounts.

---
+++ database/migrations/2017_12_13_221530_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name', 20)->unique();
+            $table->string('company', 20);
+            $table->integer('account_type')->unsigned();
+            $table->string('note', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
