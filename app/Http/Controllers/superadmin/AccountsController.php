d717ed012156fab53c5d3cf2c5c39701efc929b9
Eduardo Alfaro G2 <soporte@sistemasg2.com>
2017-12-14 13:23:46 -0600

    Return accounts and Store new Account
    Return accounts to /cuentas from index method. Store the new Account in the DB.

--- app/Http/Controllers/superadmin/AccountsController.php
+++ app/Http/Controllers/superadmin/AccountsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\superadmin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Account;
 
 class AccountsController extends Controller
 {
@@ -21,11 +22,10 @@ class AccountsController extends Controller
      */
     public function index()
     {
-        return view('superadmin.accounts', [
-          'user' => $this->user
-        ]);
+      $accounts = Account::select('name', 'company', 'key', 'note', 'type', 'avatar')->get();
+
+      return $accounts;
     }
-    
 
     /**
      * Store a newly created resource in storage.
@@ -36,11 +36,29 @@ class AccountsController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-          'name' => 'required|max:20',
+          'name' => 'required|max:20|unique:accounts',
           'company' => 'required|max:20',
-          'key' => 'required|max:16',
+          'key' => 'required|max:16|unique:accounts',
           'type' => 'required',
           'note' => 'max:255'
+        ], [
+          'name.required' => 'El campo nombre es obligatorio.',
+          'name.max' => 'El nombre no debe tener más de 20 caracteres.',
+          'company.required' => 'El campo empresa es obligatorio.',
+          'name.max' => 'La empresa no debe tener más de 20 caracteres.',
+          'key.required' => 'El campo clave es obligatorio.',
+          'key.max' => 'La clave no debe ser de más de 16 caracteres.',
+          'type.required' => 'El campo tipo de cuenta es obligatorio.',
+          'note.max' => 'Las notas no debe superar 255 caracteres.'
+        ]);
+
+        $account = Account::create([
+          'name' => $request->input('name'),
+          'company' => $request->input('company'),
+          'key' => $request->input('key'),
+          'type' => $request->input('type'),
+          'avatar' => 'https://loremflickr.com/400/400/logo',
+          'note' => $request->input('note')
         ]);
 
         return ['message' => 'Se ha creado la cuenta exitosamente!'];
