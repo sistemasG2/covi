81104e36f99ef7f08bed04404cd89e72ebf0eda7
Eduardo Alfaro G2 <soporte@sistemasg2.com>
2017-12-28 13:32:16 -0600

    Add user card in main component

--- resources/assets/js/components/superadmin/Users.vue
+++ resources/assets/js/components/superadmin/Users.vue
@@ -1,14 +1,59 @@
 <template lang="html">
-  <section class="pa-5">
-    <h1>Usuarios</h1>
-  </section>
+  <div>
+    <!-- Title -->
+    <v-layout class="grey--text mt-2 mb-2">
+      <h1>Usuarios <span class="hidden-sm-and-down">registrados</span></h1>
+      <v-spacer></v-spacer>
+      <v-btn class="ma-0" color="blue-grey" @click="openModal('create')">
+        nueva
+      </v-btn>
+    </v-layout>
+
+    <v-divider class="mt-3 mb-3"></v-divider>
+
+    <!-- Subtoolbar -->
+    <v-layout>
+      <v-btn-toggle class="elevation-0 transparent" v-model="toggleViewOptions" flat>
+        <v-btn flat small>
+          <v-icon class="subheading">fa-th-large</v-icon>
+        </v-btn>
+        <v-btn flat small>
+          <v-icon class="subheading">fa-th-list</v-icon>
+        </v-btn>
+      </v-btn-toggle>
+      <v-spacer></v-spacer>
+      <v-text-field
+        append-icon="search"
+        class="ma-0 pa-0"
+        color='cyan'
+        name="searchbox"
+        placeholder="Buscar..."
+        v-model="search"
+      ></v-text-field>
+      </v-btn>
+    </v-layout>
+
+    <section v-if="toggleViewOptions == 0">
+      <v-container class="mt-3 grid-list-sm" fluid>
+        <v-layout row wrap>
+          <user-card></user-card>
+          <user-card></user-card>
+          <user-card></user-card>
+          <user-card></user-card>
+        </v-layout>
+      </v-container>
+    </section>
+
+  </div>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      modal: false
+      toggleViewOptions: 0,
+      search: '',
     }
   }
 }
---
+++ resources/assets/js/components/superadmin/users/UserCard.vue
@@ -0,0 +1,110 @@
+<template lang="html">
+  <v-flex v-resize="onResize" xs12 sm6 md4 lg3>
+    <v-card :class="cardClass" class="user-card pa-3 pr-5">
+
+      <v-menu  transition="slide-x-transition" class="card-menu" bottom left>
+        <v-btn icon slot="activator" dark>
+          <v-icon class="grey--text">more_vert</v-icon>
+        </v-btn>
+        <v-list class="pa-0">
+          <v-list-tile>
+            <v-list-tile-title class="cyan--text text--lighten-2  body-1">
+              <v-icon class="cyan--text text--lighten-2 body-1 mr-1">fa-eye</v-icon>
+              Ver detalles
+            </v-list-tile-title>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-list-tile-title class="green--text text--lighten-2  body-1">
+              <v-icon class="green--text text--lighten-2 body-1 mr-1">fa-pencil</v-icon>
+              Editar
+            </v-list-tile-title>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-list-tile-title class="orange--text text--lighten-3  body-1">
+              <v-icon class="orange--text text--lighten-3 body-1 mr-1">vpn_key</v-icon>
+              Cambiar contraseña
+            </v-list-tile-title>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-list-tile-title class="red--text text--lighten-2  body-1">
+              <v-icon class="red--text text--lighten-2 body-1 mr-1">fa-trash-o</v-icon>
+              Eliminar
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+
+      <v-layout row>
+        <figure class="user-avatar" style="background-image: url(https://api.adorable.io/avatars/face/eyes4/nose3/mouth7/8e8895)">
+        </figure>
+        <v-layout class="ml-3 mt-0 mb-0 details" column>
+          <h1 class="subheading mb-2">@noson</h1>
+          <div>
+            <v-chip class="ma-0 mb-2 caption" color="orange" label small text-color="white">
+              Superadmin
+            </v-chip>
+          </div>
+          <small class="caption grey--text">Eduardo Alfaro</small>
+          <small class="caption green--text text--lighten-3">Sistemas G2</small>
+        </v-layout>
+      </v-layout>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      windowWidth: 0
+    }
+  },
+  methods: {
+    onResize() {
+      this.windowWidth = window.innerWidth
+    }
+  },
+  computed: {
+    cardClass() {
+      if (this.windowWidth < 600) {
+        return 'mb-3'
+      }
+
+      return 'ma-2'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .card-menu
+  {
+    position: absolute;
+    right: 0;
+    top: .6em;
+
+  }
+  .user-card
+  {
+    align-items: center!important;
+    figure
+    {
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      border-radius: 50%;
+      min-width: 100px;
+      min-height: 100px;
+      overflow: hidden;
+    }
+
+    .details
+    {
+      display: flex;
+      justify-content: center;
+    }
+  }
+</style>
