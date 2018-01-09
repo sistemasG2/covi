548149c3ea4f9f6e8e032b32c3ff9fb24749d818
Eduardo Alfaro G2 <soporte@sistemasg2.com>
2018-01-09 11:15:22 -0600

    No Result and No Data component

---
+++ resources/assets/js/components/general/NoData.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <section class="pt-5 pb-5 full-height text-xs-center">
+    <h1 class="grey--text display-2 pt-5">No hay resultados...</h1>
+    <p class="grey--text mt-4">
+      No se han encontrado registros, por favor crea uno nuevo.
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ resources/assets/js/components/general/NoResult.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="pt-5 pb-5 full-height text-xs-center">
+    <h1 class="grey--text display-2">No hay resultados...</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
